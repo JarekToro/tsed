6b6bcf42908141d963b09a696e2e3db621eaa027
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-08-31 09:38:12 +0200

    feat(platform-accept-mimes): add platform-accept-mimes to manage accept mimes as Ts.ED plugin

---
+++ packages/platform/platform-http/src/common/middlewares/PlatformAcceptMimesMiddleware.ts
@@ -0,0 +1,6 @@
+import {PlatformAcceptMimesMiddleware as M} from "@tsed/platform-accept-mimes";
+
+/**
+ * @deprecated Since version 8.16. Use PlatformAcceptMiddleware from @tsed/platform-accept-mimes package
+ */
+export const PlatformAcceptMimesMiddleware: typeof M = M;
